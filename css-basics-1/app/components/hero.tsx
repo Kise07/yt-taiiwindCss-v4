861a4b331099eb0282e7ac344f8d7d5ccd7cf000
kise <kaiazmi1412@gmail.com>
2026-06-21 16:09:47 +0530

    feat: css basics
    finta.com landing page using vanila css

---
+++ css-basics-1/app/components/hero.tsx
@@ -0,0 +1,53 @@
+export default function Hero() {
+  return (
+    <div className="hero-root">
+      <div className="badge">
+        <span>
+          What are form 1099 filing requirements?
+        </span>
+        <svg
+          viewBox="0 0 16 16"
+          width="8"
+          height="8"
+          fill="none"
+          className="badge-icon"
+        >
+          <path stroke="#1E1F25"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeOpacity=".5"
+            strokeWidth="1.25"
+            d="M8 4.75 11.25 8m0 0L8 11.25M11.25 8h-6.5">
+          </path>
+        </svg>
+      </div>
+      <h1 className="hero-title">Magically simplify
+        accounting and taxes
+      </h1>
+      <p className="hero-subtitle">Automated bookkeeping. Effortless tax filing. Financial clarity. Set up in 10 mins. Back to building by 5:23pm.</p>
+      <div className="hero-cta">
+        <button className="btn">
+          Start free trial
+        </button>
+        <button className="btn-secondary">
+          <span>Pricing</span>
+          <svg
+            viewBox="0 0 16 16"
+            width="8"
+            height="8"
+            fill="none"
+            className="badge-icon"
+          >
+            <path stroke="#1E1F25"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeOpacity=".5"
+              strokeWidth="1.25"
+              d="M8 4.75 11.25 8m0 0L8 11.25M11.25 8h-6.5">
+            </path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  )
+}
---
+++ css-basics-1/app/components/navbar.tsx
@@ -0,0 +1,31 @@
+export default function Navbar() {
+  const links = [
+    {
+      title: 'Guide',
+      href: '#',
+    },
+    {
+      title: 'Pricing',
+      href: '#',
+    },
+    {
+      title: 'Login',
+      href: '#',
+    },
+  ];
+  return (
+    <div className="navbar-root">
+      <div className="logo">Fintech</div>
+      <div className="links">
+        {links.map((link) => (
+          <a className="link-items" key={link.title} href={link.href}>
+            {link.title}
+          </a>
+        ))}
+        <button className="btn">
+          Start free trial
+        </button>
+      </div>
+    </div>
+  )
+}
