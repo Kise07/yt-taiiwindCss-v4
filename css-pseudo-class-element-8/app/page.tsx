95fc8aa656bc0ff1da61d6f0ce08db11a0f25d19
kise <kaiazmi1412@gmail.com>
2026-06-28 16:22:02 +0530

    feat:form design
    css property => HW - read the Hover and focus from tailwind page
    - :hover :focus :active
    - ::after ::before ::selection ::placeholder
    - :translate-x and -y scale [negatives]
    - :has property

---
+++ css-pseudo-class-element-8/app/page.tsx
@@ -0,0 +1,9 @@
+import { Form } from "./components/form";
+
+export default function Home() {
+  return (
+    <main className="max-w-4xl min-h-screen mx-auto flex flex-col items-center">
+      <Form />
+    </main>
+  );
+}
